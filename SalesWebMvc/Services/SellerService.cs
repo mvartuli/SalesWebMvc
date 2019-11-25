bc4d6844b4741b07ab4c1bbfbdb82aaadba5dc53
Marcello Vartuli <mvartuli@gmail.com>
2019-11-25 16:33:40 -0300

    Delete seller

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -1,8 +1,6 @@
 ﻿using SalesWebMvc.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace SalesWebMvc.Services
 {
@@ -13,7 +11,7 @@ namespace SalesWebMvc.Services
         public SellerService(SalesWebMvcContext context)
         {
             _context = context;
-        } 
+        }
 
         public List<Seller> FindAll()
         {
@@ -23,6 +21,18 @@ namespace SalesWebMvc.Services
         public void Insert(Seller obj)
         {
             _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
     }
